d58b1a707bc8b97ebd568802c5828dea080ad2d8
Owner <vitycity79@gmail.com>
2023-03-03 09:16:38 +0200

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Hillel! Lesson 01 from 28.02.2023");
     }
 }
